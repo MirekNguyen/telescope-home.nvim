32442ac22352a181cdba240dfa830a35810d1343
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-10-21 21:20:37 +0200

    Initial commit

---
+++ lua/telescope-home/init.lua
@@ -0,0 +1,29 @@
+local M = {
+	opts =  {}
+}
+
+M.default_config = {
+	level = 1,
+}
+
+local builtin = require("telescope.builtin")
+
+M.setup = function(opts)
+end
+
+M.config = function(opts)
+	M.opts = vim.tbl_deep_extend("force", default_config, opts)
+end
+
+M.find = function()
+	local level = M.opts.level
+	local current_buffer_directory = vim.fn.expand("%:h")
+
+	builtin.find_files({
+		prompt_title = "Telescope home",
+		cwd = current_buffer_directory,
+		find_command = { "find", ".", "-maxdepth", tostring(level), "-type", "f" },
+	})
+end
+
+return M
---
+++ lua/telescope/_extensions/telescope-home.lua
@@ -0,0 +1,12 @@
+local aj = require("telescope-home")
+
+return require("telescope").register_extension {
+  setup = function(ext_config, config)
+		aj.opts = vim.tbl_deep_extend("force", aj.default_config, ext_config)
+
+    -- access extension config and user config
+  end,
+  exports = {
+    telescope-home = require("telescope-home").find
+  },
+}
