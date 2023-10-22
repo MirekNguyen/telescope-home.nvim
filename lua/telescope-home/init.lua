c4a13eebdf7ab279bc76064126a2ddfa71df408c
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-10-22 21:03:55 +0200

    feat: add config

--- lua/telescope-home/init.lua
+++ lua/telescope-home/init.lua
@@ -3,6 +3,10 @@ local M = {
 }
 
 M.default_config = {
+  dir = "$HOME",
+  previewer = false,
+  hidden = true,
+  gitignore = true
 }
 
 local builtin = require("telescope.builtin")
@@ -11,25 +15,30 @@ M.setup = function(opts)
 end
 
 M.config = function(opts)
-  M.opts = vim.tbl_deep_extend("force", default_config, opts)
+  M.opts = vim.tbl_deep_extend("force", M.default_config, opts)
 end
 
 M.find = function()
   local home_directory = vim.fn.expand("$HOME")
   local ignore_file = vim.fn.expand("$HOME/.config/dotfiles/config/fdignore")
+  if M.opts.ignore_file then
+    find_command = { "fd", "--ignore-file", ignore_file, "--type", "f"}
+  else
+    find_command = { "fd", "--type", "f"}
+  end
+  if M.opts.hidden == true then
+    table.insert(find_command, "-H")
+  end
+  if M.opts.gitignore == true then
+    table.insert(find_command, "-I")
+  end
+
 
   builtin.find_files({
     prompt_title = "Telescope home",
-    cwd = home_directory,
-    find_command = { "fd", "-I", "-H", "--ignore-file", ignore_file, "--type", "f"},
-    layout_config = {
-      vertical = { 
-        width = 0.5,
-        height = 0.5
-      }
-      -- other layout configuration here
-    },
-    previewer = false
+    cwd = M.opts.dir,
+    find_command = find_command,
+    previewer = M.opts.previewer
   })
 end
 
